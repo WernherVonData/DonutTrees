bab0b3394c5582ea5573c946a6056979bb3a2d4d
danpeczek <danpeczek@gmail.com>
2021-12-17 21:50:44 +0100

    base for implementing donut gun

--- Source/IcingGun/IcingGun/Properties/AssemblyInfo.cs
+++ Source/IcingGun/IcingGun/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HelloWorld")]
+[assembly: AssemblyTitle("IcingGun")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HelloWorld")]
+[assembly: AssemblyProduct("IcingGun")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7455bc57-c04a-493f-87b9-8c9ff69598a0")]
+[assembly: Guid("4c3c33e6-9174-459e-a64c-6960f3d663e5")]
 
 // Version information for an assembly consists of the following four values:
 //
