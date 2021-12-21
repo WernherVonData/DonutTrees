33cc2677769687308410f50bde47b4c0460366dc
danpeczek <danpeczek@gmail.com>
2021-12-21 22:08:39 +0100

    Part 1 of connecting C# with XML

---
+++ Source/IcingGun/IcingGun/ModExtension_IcingBullet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcingGun
+{
+    public class ModExtension_IcingBullet : Verse.DefModExtension
+    {
+        public float addHediffChance = 0.05f;
+        public Verse.HediffDef hediffToAdd;
+    }
+}
---
+++ Source/IcingGun/IcingGun/Projectile_IcingBullet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IcingGun
+{
+    class Projectile_IcingBullet : RimWorld.Bullet
+    {
+        public ModExtension_IcingBullet Props => def.GetModExtension<ModExtension_IcingBullet>();
+
+        protected override void Impact(Verse.Thing hitThing)
+        {
+            base.Impact(hitThing);
+            if (Props != null && hitThing != null && hitThing is Verse.Pawn hitPawn)
+            {
+                float rand = Verse.Rand.Value;
+                if (rand <= Props.addHediffChance)
+                {
+                    Verse.Messages.Message("IcingBullet_Success").RimWorld.Translate(this.launcher.Label, hitPawn.Label), RimWorld.MessageTypeDefOf.NeutralEvent);
+                    Verse.Hediff icingOnPawn = hitPawn.health?.hediffSet?.GetFirstHediffOfDef(Props.hediffToAdd);
+
+                    float randomSeverity = Verse.Rand.Range(0.15f, 0.30f);
+                    if (icingOnPawn != null)
+                    {
+                        icingOnPawn.Severity += randomSeverity;
+                    } else
+                    {
+                        Verse.Hediff hediff = Verse.HediffMaker.MakeHediff(Props.hediffToAdd, hitPawn);
+                        hediff.Severity = randomSeverity;
+                        hitPawn.health.AddHediff(hediff);
+                    }
+                } else
+                {
+                    RimWorld.MoteMaker.ThrowText(hitThing.PositionHeld.ToVector3(), hitThing.MapHeld, "IcingBullet_FailureMote".Translate(Props.addHediffChance), 12f);
+                }
+            }
+        }
+    }
+}
